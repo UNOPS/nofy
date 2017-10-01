508725bf8280aae4ac5ec49af02ba33606055f66
Nik P <niaher.crimston@gmail.com>
2017-10-01 21:07:22 +0300

    refactor(test): use collection fixture

---
+++ Nofy.EntityFrameworkCore.Tests/DatabaseFixture.cs
@@ -0,0 +1,35 @@
+﻿namespace Nofy.EntityFrameworkCore.Tests
+{
+	using System.IO;
+	using Microsoft.EntityFrameworkCore;
+	using Microsoft.Extensions.Configuration;
+	using Nofy.EntityFrameworkCore.DataAccess;
+
+	public class DatabaseFixture
+	{
+		private readonly IConfigurationRoot config;
+		private readonly DbContextOptions options;
+
+		public DatabaseFixture()
+		{
+			this.config = new ConfigurationBuilder()
+				.SetBasePath(Directory.GetCurrentDirectory())
+				.AddJsonFile("appsettings.json")
+				.Build();
+
+			this.options = new DbContextOptionsBuilder().UseSqlServer(this.ConnectionString).Options;
+		}
+
+		private string ConnectionString => this.config.GetConnectionString("tag");
+
+		public NotificationsDbContext CreateDataContext()
+		{
+			return new NotificationsDbContext(this.options);
+		}
+
+		public NotificationRepository CreateRepository()
+		{
+			return new NotificationRepository(this.ConnectionString);
+		}
+	}
+}
--- Nofy.EntityFrameworkCore.Tests/MagicTest.cs
+++ Nofy.EntityFrameworkCore.Tests/MagicTest.cs
@@ -1,6 +1,5 @@
 ﻿namespace Nofy.EntityFrameworkCore.Tests
 {
-	using System;
 	using System.ComponentModel.DataAnnotations;
 	using Nofy.Core;
 	using Nofy.Core.Model;
@@ -8,28 +7,35 @@
 
 	public class MagicTest
 	{
+		public MagicTest(DatabaseFixture dbFixture)
+		{
+			this.fixture = dbFixture;
+		}
+
+		private readonly DatabaseFixture fixture;
 
 		[Fact]
 		public void ManipulateDatabase()
 		{
-			var rep = new NotificationRepository(
-				@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=notification;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+			var repository = this.fixture.CreateRepository();
 
-			var service = new NotificationService(rep);
-			var n = new Notification("test", "entityType", "entityId", "role", "1", "test",null, new NotificationAction(){ActionLink = "test",Label = "test"});
+			var service = new NotificationService(repository);
+			var n = new Notification("test", "entityType", "entityId", "role", "1", "test", null,
+				new NotificationAction() { ActionLink = "test", Label = "test" });
 			service.Publish(n);
 		}
 
 		[Fact]
 		public void Validate()
 		{
-			var rep = new NotificationRepository(
-				@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=notification;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+			var rep = this.fixture.CreateRepository();
 			var service = new NotificationService(rep);
 
 			Assert.Throws<ValidationException>(() =>
 			{
-				var n = new Notification("test", "HASIqStELYkMrXDzIkyhAH4ODJ5AIG1zNWvG3RoJLGB9SKnA9aaCwwanHvmd", "HASIqStELYkMrXDzIkyhAH4ODJ5AIG1zNWvG3RoJLGB9SKnA9aaCwwanHvmd", "role", "1", "test", null, new NotificationAction() { ActionLink = "test", Label = "test" });
+				var n = new Notification("test", "HASIqStELYkMrXDzIkyhAH4ODJ5AIG1zNWvG3RoJLGB9SKnA9aaCwwanHvmd",
+					"HASIqStELYkMrXDzIkyhAH4ODJ5AIG1zNWvG3RoJLGB9SKnA9aaCwwanHvmd", "role", "1", "test", null,
+					new NotificationAction() { ActionLink = "test", Label = "test" });
 				service.Publish(n);
 			});
 		}
