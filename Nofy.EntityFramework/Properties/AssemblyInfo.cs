20f11dc1e83b7d99c423639f2522dc60a66c2e7d
Mohammed Fuad Salem <mohammed.fuad@gmail.com>
2017-10-10 16:08:13 +0300

    generate nuget package after build

--- Nofy.EntityFramework/Properties/AssemblyInfo.cs
+++ Nofy.EntityFramework/Properties/AssemblyInfo.cs
@@ -1,16 +1,16 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Nofy.EntityFramework")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("EF target for Nofy")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("UNOPS")]
 [assembly: AssemblyProduct("Nofy.EntityFramework")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright 2017 (c) UNOPS. All rights reserved.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -34,3 +34,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("en-US")]
+
